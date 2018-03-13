c6c0ae5ddff24229eb43122be28bfbc29c9df433
Alexander Koch <github@koch.skarphet.com>
2018-03-13 15:07:10 +0100

    new approach for health check

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 
 const http = require('http');
+const port = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(301, { Location: `${process.env.REDIRECT_URL}` });
-  res.end();
+const requestHandler = (request, response) => {
+  if (request.url === '/health') {
+    response.end('ok');
+  } else {
+    response.writeHead(301, { Location: `${process.env.REDIRECT_URL}` });
+  }
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something went wrong', err);
+  }
+
+  console.log(`server is listening on ${port}`);
 });
-
-server.listen(8080);
-console.log("listening and redirecting");
