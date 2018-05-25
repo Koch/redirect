db42e3b9496ba3c908a70bcd3f525087526f44e5
Alexander Koch <github@koch.skarphet.com>
2018-05-25 08:33:23 +0200

    updated to use console.info instead of console.log

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const requestHandler = (request, response) => {
   if (request.url === '/health' || request.url === 'health') {
     response.end('ok');
   } else {
-    console.log(`forwarding request to ${process.env.REDIRECT_URL}`);
+    console.info(`forwarding request to ${process.env.REDIRECT_URL}`);
     response.writeHead(301, { Location: `${process.env.REDIRECT_URL}` });
   }
 }
@@ -15,8 +15,8 @@ const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
-    return console.log('something went wrong', err);
+    return console.info('something went wrong', err);
   }
 
-  console.log(`server is listening on ${port}`);
+  console.info(`server is listening on ${port}`);
 });
