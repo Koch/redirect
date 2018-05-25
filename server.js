40fe770b53ab30c73707845f3bccba8ef78196a3
Alexander Koch <github@koch.skarphet.com>
2018-05-25 08:45:45 +0200

    added splunk logging

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 
 const http = require('http');
 const port = process.env.PORT || 8080;
+const bunyan = require('bunyan');
+const splunkBunyan = require('splunk-bunyan-logger');
+
+const splunkStream = splunkBunyan.createStream(
+{
+  token: process.env.SPLUNK_TOKEN,
+  url: process.env.SPLUNK_URL
+});
+
+const Logger = bunyan.createLogger({
+  name: process.env.LOGGER_NAME,
+  streams: [
+    splunkStream
+  ]
+});
+
+const log = (r, m) => {
+  var payload = {
+    req: r
+  };
+
+  Logger.info(p, n)
+}
 
 const requestHandler = (request, response) => {
+  log(request, 'New request received');
   if (request.url === '/health' || request.url === 'health') {
     response.end('ok');
   } else {
-    console.info(`forwarding request to ${process.env.REDIRECT_URL}`);
     response.writeHead(301, { Location: `${process.env.REDIRECT_URL}` });
   }
 }
